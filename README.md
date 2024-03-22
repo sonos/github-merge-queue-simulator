61133d693d83e9c84863479fd57593cb3a31894c
Michal Szelag <michal.szelag@sonos.com>
2024-03-22 15:48:03 -0400

    Initial addition

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# github-merge-queue-simulator
-Tool for modeling GitHub merge queue's behavior
+
+## Overview
+Welcome to GitHub merge queue simulator.  This tool is designed to model the behavior of GitHub's merge queue and provide guidance on how to dial in various settings to optimize for throughput while balancing wait time and load on CI resources.
+
+To use this tool effectively you need to know a few things about your PR workloads.  This information comes from observing your project and analysing historical data.  The things you need to know:
+
+- Duration of the PR workload
+- Average CI failure rate
+- Target throughput
+
+Once you have a good handle on what these numbers are, you can start simulating queue behavior.
+
+Merge queue simulator is a python script that runs a large number of iterations which are meant to model GitHub's merge queue behavior using queue size, probability of failure, and randomly selecting queue position to fail a job.  The longer the duration, the more stable the predictions tend to be.  The tool computes PR throughput by tracking the number of successful PR moving through the queue over actual execution time (the last iteration may exceed allowed simulation duration).  Average wait time is computed using [Little's Law](https://en.wikipedia.org/wiki/Little%27s_law).
+
+## Quickstart
+Build the project:
+```shell
+poetry install
+```
+
+Run a simulation representing 250 hours of job executions, using queue sizes ranging from 5 to 10, where each job takes 30 minutes to execute, with a failure rate of 10%.
+```shell
+poetry run simulate --sim-duration 250 --min-queue-size 5 --max-queue-size 10 --job-duration 30 --failure-rate 0.1 
+```
+
+## Assumptions
+The simulator makes a few assumptions.
+
+- The queue is always full.
+- Delays associated with manual re-submissions when a PR is rejected are not taken into account.
+
+## How GitHub merge queue works
+GitHub merge queue uses optimistic concurrency method to optimize for throughput.  The best way to understand how this works is to visualize a scenario where a queue is building multiple jobs at the same time and a failure occurs in the middle of the queue.  Let's consider a scenario with 3 PRs:
+- `PR1` builds commit `A` which is its head commit.
+- `PR2` builds commit `B`, and it includes `PR1`'s commit `A` anticipating `PR1` build success.
+- `PR3` builds all commits `A` `B` and `C`.
+
+Queue with 3 PRs:
+```mermaid
+block-beta
+  columns 1
+  block:group1
+    columns 2
+    a["PR1"] b["commit A"]
+  end
+  block:group2
+    columns 2
+    c["PR2"] d["commit A + commit B"]
+  end
+  block:group3
+    columns 2
+    e["PR3"] f["commit A + commit B + commit C"]
+  end
+```
+
+When `PR2` build fails, it is removed from the queue, and all jobs that include its commit in the queue are restarted.
+- `PR1` is unaffected.
+- `PR2` build is removed from the queue.
+- `PR3` build is restarted with commits `A` and `C`, no commit `B`.
+
+Queue goes to 2 PRs:
+```mermaid
+block-beta
+  columns 1
+  block:group1
+    columns 2
+    a["PR1"] b["commit A"]
+  end
+  block:group2
+    columns 2
+    c["PR2"] d["commit A + commit C"]
+  end
+```
+
---
+++ sonos/github_merge_queue_simulator/simulator.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import random
+import logging
+import argparse
+import statistics
+
+logging.basicConfig(level=logging.INFO, format='[%(levelname)7s] - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def simulate_throughput(queue_size: int, sim_duration: int, job_duration: int, failure_rate: float) -> float:
+    """Compute the throughput of a queue given the queue size, duration, and failure rate.
+    
+    Args:
+        queue_size (int): Number of jobs in the queue
+        sim_duration (int): Duration of the simulation in minutes
+        job_duration (int): Duration of each job in minutes
+        failure_rate (float): Probability of a job failing expressed as a number between 0 and 1
+
+    Returns:
+        tuple: a tuple containing throughput of jobs per hour, average time to merge in minutes
+    """
+    total_jobs_completed = 0
+    total_time = 0
+    #iterations = 0
+    
+    # track a list of all wait times per iteration
+    wait_times = []
+
+    # run simulation
+    while total_time < sim_duration:
+        
+        jobs_completed = 0
+
+        # roll through the queue and pick a job to fail based on probability
+        for _ in range(1, queue_size + 1):
+            # if job failure occurs based on probability
+            if random.random() < failure_rate:
+                break
+            
+            # job completed OK
+            else:
+                jobs_completed += 1
+        
+        # track jobs completed in this iteration
+        total_jobs_completed += jobs_completed
+        
+        # increment by job execution time
+        total_time += job_duration
+
+        #iterations += 1
+
+        # can't divide by zero, this happens if the first jobs gets bounced and displaces all jobs, ignore that case
+        if jobs_completed > 0:
+            # wait time is: jobs / rate of jobs completing
+            # from Little's Law: https://en.wikipedia.org/wiki/Little%27s_law
+            wait_times.append(queue_size / (jobs_completed / job_duration))
+
+
+    # compute throughput of jobs / hour, 1 decimal point
+    throughput = round(total_jobs_completed / (total_time / 60), 1)
+
+    # compute average wait times per iteration and compute a total average
+    avg_time_to_merge = round(statistics.mean(wait_times), 1)
+
+    # compute median of the wait times per iternation
+    median_time_to_merge = round(statistics.median(wait_times), 1)
+    
+    return (throughput, avg_time_to_merge, median_time_to_merge)
+
+
+def main():
+
+    # parse the CLI args
+    parser = argparse.ArgumentParser(description="Merge queue throughput simulator.")
+    parser.add_argument("--max-queue-size", dest="max_queue_size", required=False, default=10, type=int, help="Maximum queue size to simulate.")
+    parser.add_argument("--min-queue-size", dest="min_queue_size", required=False, default=1, type=int, help="Minimum queue size to simulate.")
+    parser.add_argument("--sim-duration", "-s", dest="sim_duration", required=False, default=96, type=int, help="Simulation duration in hours.")
+    parser.add_argument("--job-duration", "-j", dest="job_duration", required=True, type=int, help="Job duration in minutes.")
+    parser.add_argument("--failure-rate", "-f", dest="failure_rate", required=True, type=float, help="Failure rate expressed as a number between 0 and 1.")
+    args = parser.parse_args()
+
+    if args.failure_rate < 0 or args.failure_rate > 1:
+        raise ValueError("Failure rate must be a number between 0 and 1.")
+    
+    # Define queue sizes to test
+    queue_sizes = range(args.min_queue_size, args.max_queue_size+1, 1)
+
+    logger.info("")
+    logger.info(f"Simulation duration: {args.sim_duration}h")
+    logger.info(f"Job duration: {args.job_duration}m")
+    logger.info(f"Failure rate: {args.failure_rate}")
+    logger.info("")
+    logger.info("Q Size | Throughput (PR/h) | Avg Time to Merge (m) | Median Time to Merge (m)")
+    logger.info("-------|-------------------|-----------------------|-------------------------")
+    # Simulate throughput for each queue size
+    for queue_size in queue_sizes:
+        throughput, avg_time_to_merge, median_time_to_merge = \
+            simulate_throughput(queue_size,
+                                args.sim_duration * 60,
+                                args.job_duration,
+                                args.failure_rate)
+        logger.info(f"{queue_size:<6} | {throughput:<17} | {avg_time_to_merge:<21} | {median_time_to_merge:<23}")
+    
+    logger.info("")
+
+
+if __name__ == "__main__":
+    exit(main())
